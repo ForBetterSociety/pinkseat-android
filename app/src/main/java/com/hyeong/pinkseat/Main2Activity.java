9c5f23f5904673d3ab37c061f4f9e2c3e88fa7af
You-hyeonyeong <dbgusdud412@naver.com>
2018-09-30 20:00:30 +0900

    com

--- app/src/main/java/com/hyeong/pinkseat/Main2Activity.java
+++ app/src/main/java/com/hyeong/pinkseat/Main2Activity.java
@@ -2,9 +2,7 @@ package com.hyeong.pinkseat;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
@@ -14,7 +12,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 import com.hyeong.pinkseat.WebView.TrafficSubwayDetail;
@@ -33,14 +30,14 @@ public class Main2Activity extends AppCompatActivity
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+       // FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         searchFragment = new SearchFragment();
         setDefaulteFragment();
