8e3f68a8dfd580f084a96e0fe5611b094a20aa9c
You-hyeonyeong <dbgusdud412@naver.com>
2018-10-07 17:48:31 +0900

    splash add

---
+++ app/src/main/java/com/hyeong/pinkseat/SplashActivity.java
@@ -0,0 +1,50 @@
+package com.hyeong.pinkseat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+            Handler hd = new Handler();
+            hd.postDelayed(new splashhandler(), 3000); // 1초 후에 hd handler 실행  3000ms = 3초
+
+        }
+
+        private class splashhandler implements Runnable {
+            public void run() {
+                startActivity(new Intent(getApplication(), ConfirmLoginActivity.class)); //로딩이 끝난 후, ChoiceFunction 이동
+                SplashActivity.this.finish(); // 로딩페이지 Activity stack에서 제거
+            }
+        }
+
+        @Override
+        public void onBackPressed () {
+            //초반 플래시 화면에서 넘어갈때 뒤로가기 버튼 못누르게 함
+        }
+    }
+
+
+
+/*
+            try {
+                ImageView rabbit = (ImageView) findViewById(R.id.gif_img);
+                GlideDrawableImageViewTarget gifImage = new GlideDrawableImageViewTarget(rabbit);
+                Glide.with(this).load(R.drawable.splash).into(gifImage);
+
+                Thread.sleep(3000);
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+*/
+
+
+            /*startActivity(new Intent(this,ConfirmLoginActivity.class));
+            finish();*/
+
